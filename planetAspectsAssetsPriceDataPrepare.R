10dcd89c7387c92d80e3e529008d614886a8576c
Pablo Cerda <pablo@citla.com>
2021-01-10 18:43:41 +0100

    Normalize data prepare scripts filenames for naming consistency

---
+++ planetAspectsAssetsPriceDataPrepare.R
@@ -0,0 +1,5 @@
+# Title     : Compose daily planetary aspects / assets price data table.
+# Objective : Prepare daily assets prices with planetary aspects data table for statistical research.
+# Created by: pablocc
+# Created on: 10/01/2021
+
