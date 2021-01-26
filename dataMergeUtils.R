2f81e135bcf42ad9c8ebeeaac8987b7e47500a3d
Pablo Cerda <pablo@citla.com>
2021-01-26 16:06:49 +0100

    Merge utility to combine planets position with asset price tables

---
+++ dataMergeUtils.R
@@ -0,0 +1,15 @@
+# Title     : Data merge utitlies.
+# Objective : Ease the combination of planets positions / aspects and asset price tables.
+# Created by: pablocc
+# Created on: 26/01/2021
+
+#' Merge daily planet longitude position and asset prices augmented tables and export to CSV.
+#' @param symbolID Symbol ID of the asset data to process.
+#' @return Boolean vector with TRUE for success data augmentation and FALSE for failed.
+planetPositionsAssetPricesDataMerge <- function(symbolID) {
+  assetPriceAugmentedFileName <- paste0(assetsDataDestinationPath(), symbolID, "--augmented.csv")
+  assetPriceAugmentedTable <- fread(assetPriceAugmentedFileName)
+  positionsFileName <- "daily_planets_positions_long"
+  dailyPlanetsPositionsTable <- fread(paste0("./data/", positionsFileName, ".csv"))
+  merge(dailyPlanetsPositionsTable, assetPriceAugmentedTable, by = c('Date'))
+}
