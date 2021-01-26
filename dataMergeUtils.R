bf0ce7eeecda7d254740f68030db0af5b8461be0
Pablo Cerda <pablo@citla.com>
2021-01-26 16:38:40 +0100

    Load libs: fix undefined functions error

--- dataMergeUtils.R
+++ dataMergeUtils.R
@@ -3,10 +3,14 @@
 # Created by: pablocc
 # Created on: 26/01/2021
 
+library(data.table)
+
+source("./fileSystemUtilities.R")
+
 #' Merge daily planet longitude position and asset prices augmented tables and export to CSV.
 #' @param symbolID Symbol ID of the asset data to process.
 #' @return Boolean vector with TRUE for success data augmentation and FALSE for failed.
-planetPositionsAssetPricesDataMerge <- function(symbolID) {
+planetsPositionsAssetPricesDataMerge <- function(symbolID) {
   assetPriceAugmentedFileName <- paste0(assetsDataDestinationPath(), symbolID, "--augmented.csv")
   assetPriceAugmentedTable <- fread(assetPriceAugmentedFileName)
   positionsFileName <- "daily_planets_positions_long"
