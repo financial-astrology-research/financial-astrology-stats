183137f2a060d15eda8c27351aadee6e619eaa2d
Pablo Cerda <pablo@citla.com>
2021-01-28 12:54:57 +0100

    Planet IDs to names mapping utility

---
+++ idsExpandUtils.R
@@ -0,0 +1,40 @@
+# Title     : IDs abbreviations expand utilities.
+# Objective : Centralize all elements IDs that are used in data tables to short column names
+#             that are hard to interpret by contributors.
+# Created by: pablocc
+# Created on: 28/01/2021
+
+library(plyr)
+
+#' Definition of all planets IDs and it's corresponding human readable names.
+planetsIdsDefinition <- function() {
+  c(
+    'Moon' = 'MO',
+    'Mercury' = 'ME',
+    'Venus' = 'VE',
+    'Sun' = 'SU',
+    'Mars' = 'MA',
+    'Vesta' = 'VS',
+    'Juno' = 'JN',
+    'Ceres' = 'CE',
+    'Pallas' = 'PA',
+    'Juno' = 'JU',
+    'North Node' = 'NN',
+    'South Node' = 'SN',
+    'Saturn' = 'SA',
+    'Chiron' = 'CH',
+    'Uranus' = 'UR',
+    'Pholus' = 'PH',
+    'Neptune' = 'NE',
+    'Pluto' = 'PL'
+  )
+}
+
+#' Map planet IDs vector to planet names.
+#' @param ids Planet IDs vector.
+#' @return Planet names vector.
+planetIdToNameMap <- function(ids) {
+  idsDefinition <- planetsIdsDefinition()
+  useIds <- idsDefinition[idsDefinition %in% ids]
+  mapvalues(ids, from = useIds, to = names(useIds))
+}
