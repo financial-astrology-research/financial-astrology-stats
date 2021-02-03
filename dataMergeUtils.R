abaea670ac5276811a855209a7fda1f1ac0c50bd
Pablo Cerda <pablo@citla.com>
2021-02-03 20:08:17 +0100

    Daily moon phases + asset price merge utility

--- dataMergeUtils.R
+++ dataMergeUtils.R
@@ -7,13 +7,24 @@ library(data.table)
 
 source("./fileSystemUtilities.R")
 
-#' Merge daily planet longitude position and asset prices augmented tables and export to CSV.
+#' Merge daily planet longitude position and asset price augmented table.
 #' @param symbolID Symbol ID of the asset data to process.
-#' @return Boolean vector with TRUE for success data augmentation and FALSE for failed.
+#' @return Daily planets position + asset prices data table.
 planetsPositionsAssetPricesDataMerge <- function(symbolID) {
   assetPriceAugmentedFileName <- paste0(assetsDataDestinationPath(), symbolID, "--augmented.csv")
   assetPriceAugmentedTable <- fread(assetPriceAugmentedFileName)
   positionsFileName <- "daily_planets_positions_long"
   dailyPlanetsPositionsTable <- fread(paste0("./data/", positionsFileName, ".csv"))
   merge(dailyPlanetsPositionsTable, assetPriceAugmentedTable, by = c('Date'))
+}
+
+#' Merge moon phases and asset price augmented table.
+#' @param symbolID Symbol ID of the asset data to process.
+#' @return Daily moon phases + asset prices data table.
+moonPhaseAssetPricesDataMerge <- function(symbolID) {
+  assetPriceAugmentedFileName <- paste0(assetsDataDestinationPath(), symbolID, "--augmented.csv")
+  assetPriceAugmentedTable <- fread(assetPriceAugmentedFileName)
+  positionsFileName <- "daily_moon_phase_positions"
+  dailyMoonPhaseTable <- fread(paste0("./data/", positionsFileName, ".csv"))
+  merge(dailyMoonPhaseTable, assetPriceAugmentedTable, by = c('Date'))
 }
