4f0e34cbb0187c375b91f5f9f8c023a44f4059e2
Pablo Cerda <pablo@citla.com>
2021-02-08 13:09:07 +0100

    Group stats/aspects load into data load utils

--- dailyMundaneEventsAssetReport.R
+++ dailyMundaneEventsAssetReport.R
@@ -31,35 +31,6 @@ frequencyStatsColumnSort <- function(reportTable, columnNames) {
   reportTable[order(get(columnNames))]
 }
 
-#' Load asset price effect frequency stats table for a given factor.
-#' @param symbolID Symbol ID to load frequency stats for.
-#' @param statsID Frequency stats identifier.
-#' @param factorID Factor column ID that was used to compute the frequency stats.
-#' @param factorParts Factor parts names to assign on ID destructure.
-#' @return Frequency stats data table.
-assetPriceEffectFrequencyStatsLoad <- function(symbolID, statsID, factorID = NULL, factorParts = NULL) {
-  sourceFileName <- paste(symbolID, statsID, "buy_sell_count_freq_stats", sep = "-")
-  statsPathFileName <- paste0(statsDataDestinationPath(symbolID), sourceFileName, ".csv")
-  frequencyTable <- copy(memoFileRead(statsPathFileName))
-  if (!is.null(factorID) & !is.null(factorParts)) {
-    frequencyTable[, c(factorParts) := tstrsplit(get(factorID), "_", fixed = T)]
-  }
-}
-
-#' Load daily planets positions data table from CSV.
-#' @return Daily planets positions data table.
-dailyMundaneEventsPositionLoad <- function() {
-  planetPositionsPathFileName <- paste0(astroDataDestinationPath(), "daily_planets_positions_long.csv")
-  memoFileRead(planetPositionsPathFileName)
-}
-
-#' Load daily planets aspects data table from CSV.
-#' @return Daily planets positions data table.
-dailyMundaneEventsAspectsLoad <- function() {
-  planetsAspectsPathFileName <- paste0(astroDataDestinationPath(), "aspects_all_planets_pablo_aspects_set_long.csv")
-  memoFileRead(planetsAspectsPathFileName)
-}
-
 #' Filter data table rows by date.
 #' @param dataTable Data table to subset from.
 #' @param filterDate Date to use for filtering.
