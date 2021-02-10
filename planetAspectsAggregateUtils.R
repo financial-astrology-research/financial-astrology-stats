fc21541fd18852eec61d391654014afd674f63bb
Pablo Cerda <pablo@citla.com>
2021-02-10 19:54:22 +0100

    Generalize aspect counts table count range grouping

--- planetAspectsAggregateUtils.R
+++ planetAspectsAggregateUtils.R
@@ -46,28 +46,35 @@ dailyPlanetReceiverAspectCount <- function(dailyAspectsTable) {
   factorDailyAggregateCount(dailyAspectsTable, "pY")
 }
 
+#' Cut in count range groups the aspect counts features.
+#' @param aspectCountTable Aspect count wide table.
+#' @param countColumnNames Count features column names.
+#' @return Aspects count table counts as range groups factors.
+aspectCountTableWideCut <- function(aspectCountTable, countColumnNames) {
+  # Group counts in cuts of 3 aspects.
+  cutLabels <- c('00-00', '01-03', '04-07', '08-20')
+  countCuts <- c(-1, 0, 3, 7, 20)
+  aspectCountTable[,
+    c(countColumnNames) := lapply(.SD, function(x) cut(x, countCuts, cutLabels, include.lowest = T)),
+    .SDcols = countColumnNames
+  ]
+}
+
 #' Prepare daily planet receiver aspects count range factors with asset augmented data.
 planetReceiverAspectsCountAssetPricePrepare <- function(symbolID) {
   dailyAspectsTable <- dailyMundaneEventsAspectsLoad()
   dailyPlanetReceiverAspectsCount <- dailyPlanetReceiverAspectCount(dailyAspectsTable)
   assetAugmentedData <- assetAgumentedDataLoad(symbolID)
   columnNames <- colnames(dailyPlanetReceiverAspectsCount)
-  selectColumns <- columnNames[2:length(columnNames)]
-
-  # Group counts in cuts of 3 aspects.
-  cutLabels <- c('00-00', '01-03', '04-07', '08-20')
-  countCuts <- c(-1, 0, 3, 7, 20)
-  dailyPlanetReceiverAspectsCount[,
-    c(selectColumns) := lapply(.SD, function(x) cut(x, countCuts, cutLabels, include.lowest = T)),
-    .SDcols = selectColumns
-  ]
+  countColumnNames <- columnNames[2:length(columnNames)]
+  aspectCountTableWideCut(dailyPlanetReceiverAspectsCount, countColumnNames)
 
   dailyPlanetReceiverAspectsCountLong <- melt(
     dailyPlanetReceiverAspectsCount,
     id.var = 'Date',
     variable.name = 'pID',
     value.name = 'CountRange',
-    measure.var = selectColumns
+    measure.var = countColumnNames,
   )
 
   merge(
