45a76e54411fbd8f1cb898d27aa9e7ea1d25177c
Pablo Cerda <pablo@citla.com>
2021-07-07 00:18:20 +0200

    Produce signals ensamble with top 7 performers models

--- cryptoModelsPredictionsIndexPrepare.R
+++ cryptoModelsPredictionsIndexPrepare.R
@@ -110,7 +110,7 @@ symbolSignalsFlattenExport <- function(signalsIndex, symbolID) {
 }
 
 symbolPredictionsIndex <- function(symbolID) {
-  topModelsN <- 5
+  topModelsN <- 7
   modelsPerformanceReport <- dataTableRead(
     modelsLatestPerformancePathFileNameGet()
   )
